fac16ecba4ab5f994641dbe4bf2705f64cae23cd
Davi Freitas <davifreitasreboucas.dev@gmail.com>
2026-07-14 18:23:44 -0300

    Remoção de alguns imports que não estavam sendo utilizados.

--- src/components/TabelaTestes.tsx
+++ src/components/TabelaTestes.tsx
@@ -2,7 +2,6 @@ import type { TesteTecido } from "../types/teste";
 import { formatarData } from "../utils/formatarData";
 import { exportarCSV } from "../utils/exportarCSV";
 import { exportarPDF } from "../utils/exportarPDF";
-import { ModalExcluir } from "../components/ModalExcluir";
 import { Loader2, Check, Database, CheckCircle2, Clock3 } from "lucide-react";
 import { FaFileCsv, FaFilePdf } from "react-icons/fa";
 import Select from "react-select";
